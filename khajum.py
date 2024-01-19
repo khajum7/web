b5d37341bd1f971cae5da354bdd5888f6a6c6508
khajum7 <xonjog97@gmail.com>
2024-01-19 13:47:32 +0545

    Update khajum.py

--- khajum.py
+++ khajum.py
@@ -2,7 +2,7 @@
 
 def main():
     print("hello ko ban and i am done")
-    print("This is the change in code so it should work yes")
+    print("This is the change in code so it should work yes no")
 
 if __name__ == "__main__":
     main()
