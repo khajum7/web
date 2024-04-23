c99d059d5d5e0f657e2c62935229b1d23a669a87
khajum7 <xonjog97@gmail.com>
2024-04-23 11:32:57 +0545

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,18 @@
-# try.py
+import json
 
-def main():
-    print("hello ko ban and i am done")
-    print("This is the change in code so it should work yes it is")
-    print("if it work")
-if __name__ == "__main__":
-    main()
+def lambda_handler(event, context):
+    # Retrieve data from the event
+    message = event['message']
+    
+    # Process the message
+    processed_message = message.upper()
+    
+    # Construct the response
+    response = {
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': processed_message
+        })
+    }
+    
+    return response
