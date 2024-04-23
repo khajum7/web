eb7c425d5f34bc244b214bff7351e875f5eb7a1d
khajum7 <xonjog97@gmail.com>
2024-04-23 13:37:23 +0545

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,6 @@ import json
 def lambda_handler(event, context):
     print("event",event)
     print("fail")
-    print("k vako hau")
     # Retrieve data from the event
     message = event['message']
     
