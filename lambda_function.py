74240c29bb50da4a7118821be647e0102e39809f
khajum7 <xonjog97@gmail.com>
2024-04-23 11:29:56 +0545

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,6 @@
 def main():
     print("hello ko ban and i am done")
     print("This is the change in code so it should work yes it is")
-
+    print("if it work")
 if __name__ == "__main__":
     main()
