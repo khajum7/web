83dbe6d1e3588146fad3c38d6195d4bd661294a6
Sanjog Limbu <xonjog97@gmail.com>
2025-02-18 13:54:52 +0545

    Update khajum.py

--- khajum.py
+++ khajum.py
@@ -2,7 +2,7 @@
 
 def main():
     print("this workgs")
-    print("This is the change in code so it should work yes it is")
+    print("This is the changes in code so it should work yes it is")
 
 if __name__ == "__main__":
     main()
