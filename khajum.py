f01043162150ea3ec633e73eda37215f9f901906
Sanjog Limbu <xonjog97@gmail.com>
2025-06-05 10:49:34 +0545

    Update khajum.py

--- khajum.py
+++ khajum.py
@@ -3,7 +3,7 @@
 def main():
 
     print("this won't workss")
-    print("This is the change in code so it should work yes it is"
+    print("This is the chasnge in code so it should work yes it is"
 
     print("this workgs")
     print("This is the changes in code so it should work yes it is")
