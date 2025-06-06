90b3244ab844f786cd1745e209a680f1ded6005e
Sanjog Limbu <xonjog97@gmail.com>
2025-06-06 21:16:08 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_db_cluster_snapshot" "vivint-snapshot" {
-  db_cluster_identifier          = "vivint-store"
-  db_cluster_snapshot_identifier = "snapshot-vivint-terraform"
+resource "aws_db_cluster_snapshot" "voxecommerce-cluster-snapshot" {
+  db_cluster_identifier          = "voxecommerce-cluster"
+  db_cluster_snapshot_identifier = "voxecommerce-cluster-terraform"
 }
