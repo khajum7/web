bc9d773dfdafcaa81621b533301af60edfb71ddd
Sanjog Limbu <xonjog97@gmail.com>
2025-06-06 20:43:03 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,15 +2,7 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_db_instance" "vivint_store_instance" {
-  identifier             = "vivint-store-instance-1"
-  instance_class         = "db.t3.medium"   # use the actual class of your existing RDS instance
-  allocated_storage      = 1               # dummy value; not used during import
-  engine                 = "aurora-mysql"         # placeholder; won't overwrite real settings on import
-  skip_final_snapshot    = true
-}
-
-resource "aws_db_snapshot" "snapshot_vivint" {
-  db_instance_identifier = aws_db_instance.vivint_store_instance.id
-  db_snapshot_identifier = "snapshot-vivint-final"
+resource "aws_rds_cluster_snapshot" "snapshot_vivint_cluster" {
+  db_cluster_identifier           = "vivint-store"
+  db_cluster_snapshot_identifier = "snapshot-vivint-cluster-final"
 }
