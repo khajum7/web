3a38b64c793eb3db4ca86c7b3380795852b5c636
Sanjog Limbu <xonjog97@gmail.com>
2025-06-06 16:38:50 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,12 @@ provider "aws" {
   region = "us-west-1"
 }
 
+resource "aws_db_instance" "vivint_store_instance" {
+  # Minimal placeholder for import reference
+  skip_final_snapshot = true
+}
 
-resource "aws_db_snapshot" "Snapshot-vivnt" {
-  db_instance_identifier       = aws_db_instance.vivint-store-instance-1.identifier
-  db_snapshot_identifier       = "Backup-vivint"
+resource "aws_db_snapshot" "snapshot_vivint" {
+  db_instance_identifier = aws_db_instance.vivint_store_instance.id
+  db_snapshot_identifier = "snapshot-vivint-final"
 }
