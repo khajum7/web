b725b871e6b6b163ebd0ee5cb8390a8f5056aa3f
Sanjog Limbu <xonjog97@gmail.com>
2025-06-06 21:01:59 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_db_cluster_snapshot" "snapshot_vivint_cluster" {
-  db_cluster_identifier           = "vivint-store"
-  db_cluster_snapshot_identifier = "snapshot-vivint-cluster-${replace(timestamp(), "/[^a-zA-Z0-9]/", "-")}"
+resource "aws_db_cluster_snapshot" "vivint-snapshot" {
+  db_cluster_identifier          = "vivint-store"
+  db_cluster_snapshot_identifier = "snapshot-vivint"
 }
