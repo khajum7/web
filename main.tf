7d037b0dda0a0f64194983daea8572d89727772c
Sanjog Limbu <xonjog97@gmail.com>
2025-06-06 16:09:07 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,18 @@
-provider "aws" {
-  region = "us-west-1"
+resource "aws_db_instance" "vivint-store-instance-1" {
+  allocated_storage       = 1
+  engine                  = "aurora-mysql"
+  engine_version          = "8.0.mysql_aurora.3.08.2"
+  instance_class          = "db.t3.medium"
+  db_name                 = "vivintStore"
+  password                = "e0;M1r~=<Pm4==F}scTuP[j?)*j4qPmn"
+  username                = "vivintStore"
+
+  maintenance_window      = "Fri:09:00-Fri:09:30"
+  backup_retention_period = 0
+  parameter_group_name    = "voxmg8-pg"
 }
 
-resource "aws_rds_cluster" "vivint_store" {
-  cluster_identifier      = "vivint-store"
-  engine                  = "aurora-mysql"
-  engine_version          = "8.0.mysql_aurora.3.08.2"  # Upgrade target
-  master_username         = "vivintStore"
-  db_subnet_group_name    = "stg-db-sg"
-  vpc_security_group_ids  = ["sg-0cc12cef7ededca5c"]
-  storage_encrypted       = true
-  kms_key_id              = "arn:aws:kms:us-west-1:174687093387:key/4754ff9f-6dde-4957-aac3-5462c64c1377"
-  backup_retention_period = 1
-  preferred_backup_window      = "07:30-08:00"
-  preferred_maintenance_window = "fri:06:00-fri:06:30"
-  deletion_protection     = false
-  enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
-
-  apply_immediately       = true
+resource "aws_db_snapshot" "Snapshot-vivnt" {
+  db_instance_identifier       = aws_db_instance.vivint-store-instance-1.identifier
+  db_snapshot_identifier       = "Backup-vivint"
 }
