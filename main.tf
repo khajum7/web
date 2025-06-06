f5a422f333f9e92d039b7a955681d41c957889c4
Sanjog Limbu <xonjog97@gmail.com>
2025-06-06 15:31:34 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_rds_cluster" "vivint_store" {
   cluster_identifier      = "vivint-store"
   engine                  = "aurora-mysql"
-  engine_version          = "8.0.mysql_aurora.3.08.2"  # Upgrade target
+  engine_version          = "8.0.mysql_aurora.3.05.2"  # Upgrade target
   master_username         = "vivintStore"
   db_subnet_group_name    = "stg-db-sg"
   vpc_security_group_ids  = ["sg-0cc12cef7ededca5c"]
@@ -17,20 +17,5 @@ resource "aws_rds_cluster" "vivint_store" {
   deletion_protection     = false
   enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
 
-  apply_immediately       = true
-}
-
-resource "aws_rds_cluster_instance" "vivint_store_instance_1" {
-  identifier              = "vivint-store-instance-1"
-  cluster_identifier      = "vivint-store"
-  instance_class          = "db.t3.medium"
-  engine                  = "aurora-mysql"
-  db_subnet_group_name    = "stg-db-sg"
-  publicly_accessible     = false
-  auto_minor_version_upgrade = true
-  monitoring_interval     = 60
-  monitoring_role_arn     = "arn:aws:iam::174687093387:role/rds-monitoring-role"
-  performance_insights_enabled     = false
-  performance_insights_kms_key_id = "arn:aws:kms:us-west-1:174687093387:key/4754ff9f-6dde-4957-aac3-5462c64c1377"
   apply_immediately       = true
 }
