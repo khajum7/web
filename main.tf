0ccd4c46e6e95579e5512b209607e0a640748943
Sanjog Limbu <xonjog97@gmail.com>
2025-06-06 20:49:34 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_rds_cluster_snapshot" "snapshot_vivint_cluster" {
-  db_cluster_identifier           = "vivint-store"
-  db_cluster_snapshot_identifier = "snapshot-vivint-cluster-final"
+resource "aws_db_cluster_snapshot" "snapshot_vivint_cluster" {
+  db_cluster_identifier           = "vivint-store" # your Aurora cluster ID
+  db_cluster_snapshot_identifier = "snapshot-vivint-cluster-${timestamp()}"
 }
