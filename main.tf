4c8735a40713848f198384a686e828881b927338
Sanjog Limbu <xonjog97@gmail.com>
2025-06-10 13:57:26 +0545

    Delete main.tf

--- main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "us-west-1"
-}
-
-resource "aws_db_cluster_snapshot" "voxecommerce-cluster-snapshot" {
-  db_cluster_identifier          = "voxecommerce-cluster"
-  db_cluster_snapshot_identifier = "voxecommerce-cluster-terraform"
-}
