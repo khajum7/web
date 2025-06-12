6f0b96c254cc567018e8c935c2aff288e43f1fb0
Sanjog Limbu <xonjog97@gmail.com>
2025-06-12 13:58:11 +0545

    Update services.php

--- config/services.php
+++ config/services.php
@@ -23,10 +23,6 @@ return [
     ],
 
     'sns' => [
-        'region' => env('AWS_DEFAULT_REGION', 'us-west-1'),
-    ],
-
-    'sns' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
